af395cfb62f24bd898f86d7598ce9a97e646c9ae
TMN <t.martin@adsoftware.fr>
2022-10-19 18:40:26 +0200

    fix: fix

--- components/SessionDatagrid.tsx
+++ components/SessionDatagrid.tsx
@@ -63,11 +63,13 @@ export const SessionDatagrid = ({ session }: Props) => {
           alignment="center"
           cellRender={(e) => e.rowIndex + 1}
           fixed={session.mode === "TRAINING"}
+          fixedPosition="left"
         />
         <Column
           caption="Coureur"
           dataField={"athleteName"}
           fixed={session.mode === "TRAINING"}
+          fixedPosition="left"
         />
         <Column
           dataField={"m1"}
