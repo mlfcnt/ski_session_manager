c76c5d4b7e934a41398c13f57cbe1aacc498c677
TMN <t.martin@adsoftware.fr>
2022-10-23 09:46:27 +0200

    test

--- components/TimingInput.tsx
+++ components/TimingInput.tsx
@@ -1,13 +1,15 @@
-import { TextInput, TextInputProps } from "@mantine/core";
+import { NumberInputProps, TextInput, TextInputProps } from "@mantine/core";
 import React from "react";
+import ReactCodeInput from "react-code-input";
 
-export const TimingInput = ({ ...props }: TextInputProps) => {
+export const TimingInput = ({ ...props }: any) => {
   return (
-    <TextInput
-      label="temps"
-      description="Exemple du format attendu: 01.59.99"
-      variant="filled"
-      {...props}
-    />
+    // <TextInput
+    //   label="temps"
+    //   description="Exemple du format attendu: 01.59.99"
+    //   variant="filled"
+    //   {...props}
+    // />
+    <ReactCodeInput type="number" fields={6} {...props} />
   );
 };
